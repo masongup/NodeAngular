469a9a118f0c1964574a89dd914de9d2d1e4725c
Mason Gup <masongup@gmail.com>
2016-01-18 23:11:09 -0600

    Keep authors in a separate table

--- public/site.js
+++ public/site.js
@@ -2,6 +2,14 @@ angular.module('SiteApp', ['ngRoute'])
   .constant('ServerUrl',  'http://localhost:3000/')
   .config(['$routeProvider', '$locationProvider', 'ServerUrl', function($routeProvider, $locationProvider, serverUrl) {
     $locationProvider.html5Mode(true);
+    var bookQuery = ['$http', '$route', function($http, $route) { 
+            return $http.get(serverUrl + 'books?id=eq.' + $route.current.params.bookNum + '&select=*,authors{*}', {
+              headers: { Prefer: 'plurality=singular' }
+            });
+          }];
+    var authorsQuery = ['$http', function($http) {
+      return $http.get(serverUrl + 'authors', { headers: { Range: '0-50' } });
+    }];
     $routeProvider
       .when('/', {
         templateUrl: 'main.html',
@@ -13,30 +21,25 @@ angular.module('SiteApp', ['ngRoute'])
         controller: 'BookEditController',
         controllerAs: 'bec',
         resolve: {
-          book: ['$http', '$route', function($http, $route) { 
-            return $http.get(serverUrl + 'books?id=eq.' + $route.current.params.bookNum, {
-              headers: { Prefer: 'plurality=singular' }
-            });
-          }]
+          book: bookQuery,
+          authors: authorsQuery
         }
       })
       .when('/book/new', {
         templateUrl: 'bookEdit.html',
         controller: 'BookEditController',
         controllerAs: 'bec',
-        resolve: { book: function() { return null; } }
+        resolve: { 
+          book: function() { return null; } ,
+          authors: authorsQuery
         }
-      )
+      })
       .when('/book/:bookNum', {
         templateUrl: 'book.html',
         controller: 'BookController',
         controllerAs: 'bc',
         resolve: {
-          book: ['$http', '$route', function($http, $route) { 
-            return $http.get(serverUrl + 'books?id=eq.' + $route.current.params.bookNum, {
-              headers: { Prefer: 'plurality=singular' }
-            });
-          }]
+          book: bookQuery
         }
       })
       .when('/book', {
@@ -45,7 +48,9 @@ angular.module('SiteApp', ['ngRoute'])
         controllerAs: 'blc',
         resolve: {
           books: ['$http', function($http) {
-            return $http.get(serverUrl + 'books');
+            return $http.get(serverUrl + 'books?select=id,title,authors{id,name}&order=id', {
+              headers: { Range: '0-50' }
+            });
           }]
         }
       });
@@ -64,8 +69,10 @@ angular.module('SiteApp', ['ngRoute'])
   .controller('BookListController', ['books', function(books) {
     this.books = books.data;
   }])
-  .controller('BookEditController', ['book', '$http', '$location', 'ServerUrl', function(book, $http, $location, serverUrl) {
+  .controller('BookEditController', ['book', 'authors', '$http', '$location', 'ServerUrl',
+      function(book, authors, $http, $location, serverUrl) {
       var requestOptions = {};
+      this.authors = authors.data;
       if (book) {
         this.book = book.data;
         this.title = 'Edit Book';
@@ -80,10 +87,17 @@ angular.module('SiteApp', ['ngRoute'])
       }
       requestOptions.data = this.book;
       this.save = function() {
+        var bookId = this.book.id;
+        this.book.author_id = this.book.authors.id;
+        delete this.book.authors;
         $http(requestOptions).then(function(data) {
           var header = data.headers('Location');
-          if (header)
+          if (header) {
             $location.url('/book/' + /\d+$/.exec(header)[0]);
-          });
+          }
+          else {
+            $location.url('/book/' + bookId);
+          }
+        });
       };
   }]);
