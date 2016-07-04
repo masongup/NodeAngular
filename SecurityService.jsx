1b446dfe9c42dec361fec2eb12850508bf4af01c
Mason Gup <masongup@gmail.com>
2016-07-04 12:14:54 -0500

    Start working with Redux for the Security Service

--- SecurityService.jsx
+++ SecurityService.jsx
@@ -1,4 +1,28 @@
 const { serverUrl } = require('./consts.js');
+const { createStore } = require('redux');
+
+const initialState = {
+  role: null,
+  userName: null,
+  canEdit: false,
+  token: null
+};
+
+function appReducer(state = initialState, action) {
+  switch (action.type) {
+    case 'SetLoginState':
+      return Object.assign({}, state, {
+        role: action.role,
+        userName: action.userName,
+        canEdit: action.role === 'editor',
+        token: action.token
+      });
+    default:
+      return state;
+  }
+}
+
+const store = createStore(appReducer);
 
 class SecurityService {
 
@@ -21,40 +45,44 @@ class SecurityService {
       });
   }
 
+  createLoginChangeAction(newRole, userName, token) {
+    return {
+        type: 'SetLoginState',
+        role: newRole,
+        userName: userName,
+        token: token
+    };
+  }
+
   logout() {
-    this.isLoggedIn = false;
-    this.role = null;
-    this.userName = null;
     //localStorageService.remove('Token');
+    store.dispatch(this.createLoginChangeAction());
   }
 
   getIsLoggedIn() {
-    return this.isLoggedIn;
+    return store.getState().role !== null;
   }
   canEdit() {
-    return this.role === 'editor';
+    return store.getState().role === 'editor';
   }
   getUserName() {
-    return this.userName;
+    return store.getState().userName;
   }
   getRole() {
-    return this.role;
+    return store.getState().role;
   }
   getToken() {
-    return this.token;
+    return store.getState().token;
   }
 
   processToken(t) {
     const dataPart = t.split('.')[1];
     const data = JSON.parse(atob(dataPart));
-    this.userName = data.username;
-    this.role = data.role;
-    if (!this.userName || !this.role) {
+    if (!data.username || !data.role) {
       this.logout();
       return;
     }
-    this.token = t;
-    this.isLoggedIn = true;
+    store.dispatch(this.createLoginChangeAction(data.role, data.username, t));
   }
 }
 
